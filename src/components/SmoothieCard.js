68c4928771634d3f94c953e071e65957d05f200d
simonbeirouti <hello@simonbeirouti.com>
2022-10-05 20:53:11 +1100

    configure delete button to rmv entry from db

--- src/components/SmoothieCard.js
+++ src/components/SmoothieCard.js
@@ -1,6 +1,21 @@
 import { Link } from "react-router-dom";
+import supabase from "../config/supabaseClient";
 
 export default function SmoothieCard({ smoothie }) {
+  const handleDelete = async () => {
+    const { data, error } = await supabase
+      .from("smoothies")
+      .delete()
+      .eq("id", smoothie.id);
+
+    if (error) {
+      console.log(error);
+    }
+    if (data) {
+      console.log(data);
+    }
+  };
+
   return (
     <div className="smoothie-card">
       <h3>{smoothie.title}</h3>
@@ -9,8 +24,10 @@ export default function SmoothieCard({ smoothie }) {
       <div className="buttons">
         <Link to={"/" + smoothie.id}>
           <i className="material-icons">edit</i>
-          <i className="material-icons">delete</i>
         </Link>
+        <i className="material-icons" onClick={handleDelete}>
+          delete
+        </i>
       </div>
     </div>
   );
