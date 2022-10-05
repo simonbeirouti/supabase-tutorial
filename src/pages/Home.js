0c651b67d9e7abd864c5594a76838d77232c1c97
simonbeirouti <hello@simonbeirouti.com>
2022-10-05 15:36:10 +1100

    create the card and pull data from supabase

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,53 @@
+import supabase from "../config/supabaseClient";
+import { useEffect, useState } from "react";
+import SmoothieCard from "../components/SmoothieCard";
+
 export default function Home() {
+  const [fetchError, setFetchError] = useState(null);
+  const [smoothies, setSmoothies] = useState(null);
+
+  // Fetching the smoothies from the database
+  useEffect(() => {
+    // Function to fetch the smooothis from the database
+    const fetchSmoothies = async () => {
+      // We get two things back, data and error
+      const { data, error } = await supabase
+        // Choosing the table
+        .from("smoothies")
+        // Selecting all results
+        .select("*");
+      // If there is an error, we set the error state
+      if (error) {
+        // Return an custom message
+        setFetchError("Could not fetch the smoothies");
+        // Set smoothies to null (in case there is values already)
+        setSmoothies(null);
+      }
+      // If there is no error, we set the smoothies state
+      if (data) {
+        // Set the smoothies state to the smoothies we got back
+        setSmoothies(data);
+        // Set the error state to null (in case there is an error already)
+        setFetchError(null);
+      }
+    };
+    // Calling the function to get the data
+    fetchSmoothies();
+  }, []);
+
   return (
     <div className="page home">
-      <h2>Home</h2>
+      {fetchError && <p>{fetchError}</p>}
+      {smoothies && (
+        <div className="smoothies">
+          {/* Buttons to order smoothies */}
+          <div className="smoothie-grid">
+            {smoothies.map((smoothie) => (
+              <SmoothieCard key={smoothie.id} smoothie={smoothie} />
+            ))}
+          </div>
+        </div>
+      )}
     </div>
   );
 }
